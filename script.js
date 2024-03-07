5d0106f21a8b7ca37f7a33c9fd3c4f2c4701258e
Joydev2000 <joydevsuvo2202@gmail.com>
2024-03-08 01:06:18 +0530

    k

--- script.js
+++ script.js
@@ -14,16 +14,31 @@ function myFunction(x) {
   }
 
 
-var swiper = new Swiper('.swiper-container.swiper-testimonial', {
-    spaceBetween: 30,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
+  const swiperTestmonials = new Swiper('.swiper-testmonials', {
+    loop: true,
+    slidesPerView: 1.2,
+    grabCursor: true,
+    // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next-test',
         prevEl: '.swiper-button-prev-test',
     },
+    breakpoints: {
+        // when window width is >= 640px
+        500: {
+            slidesPerView: 1.4,
+        },
+        780: {
+            slidesPerView: 1.8,
+        },
+        1300: {
+            slidesPerView: 2.6,
+        },
+        1630: {
+            slidesPerView: 3.2,
+        }
+    }
+
 });
 
 
@@ -41,4 +56,11 @@ $(document).ready(function () {
       $("body").css("overflow", "auto");
     });
   
+  });
+
+
+  $(document).ready(function() {
+    if ($(window).width() <= 715) {
+        $(".left, .right").attr("data-aos-delay", "0");
+    }
   });
